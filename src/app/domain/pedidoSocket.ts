87dd532a37bde1ad4b8eb5ad1e7114eec0f8095a
Raphael Kieling <rapha_kieling@hotmail.com>
2018-04-13 15:41:43 -0300

    pedidoSocket

---
+++ src/app/domain/pedidoSocket.ts
@@ -0,0 +1,8 @@
+import { Cliente } from './cliente';
+import { Pedido } from './pedido';
+import { Categoria } from './categoria';
+export class PedidoSocket {
+    categoria: Categoria;
+    pedido: Pedido;
+    cliente: Cliente;
+}
