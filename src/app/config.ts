454114d0adb0b2761691ce621e6f977d78cfc6db
raphael kieling <rapha_kieling@hotmail.com>
2018-06-13 00:35:10 -0300

    Ajustado nome com /

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 // https://thawing-bastion-86753.herokuapp.com/graphql
 
 // http://localhost:3000/graphql
-export const urlDomain = 'https://findmeapi.herokuapp.com/'
+export const urlDomain = 'https://findmeapi.herokuapp.com'
